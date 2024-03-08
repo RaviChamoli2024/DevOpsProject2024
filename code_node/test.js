c30c4ff48c69b058dd5ec0bb06b11dc38ecd94e9
RaviChamoli2024 <154981247+RaviChamoli2024@users.noreply.github.com>
2024-03-08 18:47:03 +0530

    Update test.js

--- code_node/test.js
+++ code_node/test.js
@@ -1,6 +1,8 @@
 if ("cat" == "dog") {
-      var outcome = "if block";
+     // var outcome = "if block";  @for error
+      let outcome = "if block";
 } else {
-      var outcome = "else block"
+      // var outcome = "else block" @for error
+      let outcome = "else block";
 }
-outcome;    
+// outcome; for error
