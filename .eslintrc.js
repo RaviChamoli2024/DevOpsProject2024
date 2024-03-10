e362cc7a21fa5908b3126c07b4547bacfe25624c
RaviChamoli2024 <154981247+RaviChamoli2024@users.noreply.github.com>
2024-03-10 21:36:15 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,35 @@
 module.exports = {
-  overrides: [
-    {
-      files: ['*.js', '*.ts'],
-      extends: 'eslint-config-standard-with-typescript',
-      parserOptions: {
-        project: './tsconfig.json'
-      }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
     }
-  ]
 }
